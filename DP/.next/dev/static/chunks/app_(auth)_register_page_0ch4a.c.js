32e20bc8f8ffd4d473dc040d458d9c7ab2c51ffe
miracorner <106338183+Mirahodec@users.noreply.github.com>
2026-05-17 16:57:53 +0300

    Удаляем Next
    Next нельзя загрузить на Гит

--- DP/.next/dev/static/chunks/app_(auth)_register_page_0ch4a.c.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0zjlgdq._.js","static/chunks/_11tz393._.js"],
-    source: "dynamic"
-});
--- DP/.next/dev/static/chunks/app_(main)_dashboard_page_0ch4a.c.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0tz.kyk._.js","static/chunks/node_modules_0ljxxo7._.js"],
-    source: "dynamic"
-});
